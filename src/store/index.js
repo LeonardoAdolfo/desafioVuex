5e3a43aea8165ca57a0c16f9a560b5f94de31b91
LeonardoAdolfo <78151545+LeonardoAdolfo@users.noreply.github.com>
2021-09-13 11:37:20 -0300

    13.09.2021

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,70 @@
-import { createStore } from "vuex";
-import { faqCategories } from "@/utils/db.json"
+import { createStore } from 'vuex'
+import { faqCategories } from '../utils/db.json'
 
 export default createStore({
   state: {
     faqCategories: [],
-    questionsCategory: {},
-    answerQuestion: {},
-    currentComponent: 'faqCategories',
-    page: 1
+    singleCatagory: {},
+    singleQuestion: {},
+    currentComponent: 'FaqCategories',
+    transitionDepth: 1
   },
   mutations: {
     SET_FAQ_CATEGORIES(state, data) {
       state.faqCategories = data
     },
-    INCREASE_PAGE(state) {
-      state.page++
-    },
-    DECREASE_PAGE(state) {
-      state.page--
-    },
-    SET_NEW_COMPONENT(state, payload){
+    SET_CURRENT_COMPONENT(state, payload) {
       state.currentComponent = payload
     },
-    SET_QUESTION_CATEGORY(state, payload){
-      state.questionsCategory = payload
+    SET_SINGLE_CATEGORY(state, payload) {
+      state.singleCatagory = payload
     },
-    SET_ANSWER_QUESTION(state, payload){
-      state.answerQuestion = payload
+    SET_SINGLE_QUESTION(state, payload) {
+      state.singleQuestion = payload
     },
+    INCREASE_TRANSITION_DEPTH(state) {
+      state.transitionDepth++
+    },
+    DECREASE_TRANSITION_DEPTH(state) {
+      state.transitionDepth--
+    }
   },
   actions: {
     fetchFaqCategorias({ commit }) {
       const data = faqCategories
-
-      commit('SET_FAQ_CATEGORIES', payload)
+      commit('SET_FAQ_CATEGORIES', data)
     },
-    
-
+    changeCurrentComponent({ commit }, payload) {
+      commit('SET_CURRENT_COMPONENT', payload)
+    },
+    increaseTransitonDepth({ commit }) {
+      commit('INCREASE_TRANSITION_DEPTH')
+    },
+    decreaseTransitonDepth({ commit }) {
+      commit('DECREASE_TRANSITION_DEPTH')
+    },
+    getSingleCategory({ commit }, payload) {
+      commit('SET_SINGLE_CATEGORY', payload)
+    },
+    getSingleQuestion({ commit }, payload) {
+      commit('SET_SINGLE_QUESTION', payload)
+    }
   },
   getters: {
-
-  },
-});
+    $currentComponent(state) {
+      return state.currentComponent
+    },
+    $allCategories(state) {
+      return state.faqCategories
+    },
+    $singleCategory(state) {
+      return state.singleCatagory
+    },
+    $singleQuestion(state) {
+      return state.singleQuestion
+    },
+    $transitionDepth(state) {
+      return state.transitionDepth
+    }
+  }
+})
